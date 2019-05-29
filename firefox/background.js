46596d5a61f8b3e53d283072e4637edf1227bdb9
Wesley Branton <WesleyBranton@users.noreply.github.com>
2019-05-28 23:48:59 -0400

    Added version 1.4

---
+++ firefox/background.js
@@ -0,0 +1,47 @@
+function load() {
+	var d = new Date();
+	var year = d.getFullYear();
+	var month = d.getMonth();
+	month += 1;
+	var date = d.getDate();
+	var hour = d.getHours();
+	var dateString = year + '-' + month + '-' + date + 'T' + hour + ':00:00';
+	var d = Date.parse(dateString);
+	d += 3600000;
+	browser.alarms.create({when:d,periodInMinutes:60});
+}
+
+function hourTrigger() {
+	browser.storage.local.get('chime', (res) => {
+		var chimeName = res.chime;
+		var d = new Date();
+		var hour = d.getHours();
+		if (hour > 12) {
+			hour = hour - 12;
+		} else if (hour == 0) {
+			hour = 12;
+		}
+		var audio = new Audio('audio/' + chimeName + '/' + hour + '.ogg');
+		audio.play();
+	});
+}
+
+function handleInstalled(details) {
+	if (details.reason == 'install') {
+		browser.storage.local.set({
+			chime: 'default'
+		});
+	}
+}
+
+function listenMessage(msg) {
+	if (msg == 'reload') {
+		var clearAlarms = browser.alarms.clearAll();
+		clearAlarms.then(load);
+	}
+}
+
+browser.runtime.onInstalled.addListener(handleInstalled);
+browser.alarms.onAlarm.addListener(hourTrigger);
+chrome.runtime.onMessage.addListener(listenMessage);
+load();
---
+++ firefox/options/options.js
@@ -0,0 +1,42 @@
+function saveOptions() {
+	var setting = document.getElementById('chimeNoise').value;
+	browser.storage.local.set({'chime': setting});
+}
+
+function restoreOptions() {
+	browser.storage.local.get('chime', (res) => {
+		var setting = document.getElementById('chimeNoise');
+		setting.value = res.chime;
+	});
+}
+
+function playAudio() {
+	var sound = document.getElementById('tower');
+	sound.pause();
+	sound.currentTime = 0;
+	sound = document.getElementById('cuckoo');
+	sound.pause();
+	sound.currentTime = 0;
+	sound = document.getElementById('default');
+	sound.pause();
+	sound.currentTime = 0;
+	sound = document.getElementById('multibeep');
+	sound.pause();
+	sound.currentTime = 0;
+	sound = document.getElementById('singlebeep');
+	sound.pause();
+	sound.currentTime = 0;
+	
+	var audioType = document.getElementById('chimeNoise').value;
+	var x = document.getElementById(audioType);
+    x.play();
+}
+
+function reload() {
+	chrome.runtime.sendMessage('reload');
+}
+
+restoreOptions();
+document.getElementsByTagName("form")[0].addEventListener("change", saveOptions);
+document.getElementById('sample').addEventListener('click',playAudio);
+document.getElementById('reload').addEventListener('click',reload);
