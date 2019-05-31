ee3e4e9d78d34da0a1f7eb4192de72dc985f1a38
Wesley Branton <addons@wesleybranton.com>
2019-05-31 17:58:02 -0400

    Added license headers

--- firefox/options/options.js
+++ firefox/options/options.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 function saveOptions() {
 	var setting = document.getElementById('chimeNoise').value;
 	browser.storage.local.set({'chime': setting});
