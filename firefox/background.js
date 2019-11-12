43009cabd985d63715be663da4bcea203760414e
Wesley Branton <addons@wesleybranton.com>
2019-11-12 13:24:48 -0500

    Save chime setting into background.js for better performance

--- firefox/background.js
+++ firefox/background.js
@@ -18,18 +18,15 @@ function load() {
 }
 
 function hourTrigger(alarmInfo) {
-	browser.storage.local.get('chime', (res) => {
-		var chimeName = res.chime;
-		var d = new Date(alarmInfo.scheduledTime);
-		var hour = d.getHours();
-		if (hour > 12) {
-			hour = hour - 12;
-		} else if (hour == 0) {
-			hour = 12;
-		}
-		var audio = new Audio('audio/' + chimeName + '/' + hour + '.ogg');
-		audio.play();
-	});
+	var d = new Date(alarmInfo.scheduledTime);
+	var hour = d.getHours();
+	if (hour > 12) {
+		hour = hour - 12;
+	} else if (hour == 0) {
+		hour = 12;
+	}
+	var audio = new Audio('audio/' + chimeName + '/' + hour + '.ogg');
+	audio.play();
 }
 
 function handleInstalled(details) {
@@ -47,7 +44,21 @@ function listenMessage(msg) {
 	}
 }
 
+function storageChange(changes) {
+	if (changes.chime) {
+		chimeName = changes.chime.newValue;
+	}
+}
+
+async function firstLoad() {
+	var setting = await browser.storage.local.get('chime');
+	chimeName = setting.chime;
+}
+
+var chimeName;
+firstLoad();
 browser.runtime.onInstalled.addListener(handleInstalled);
 browser.alarms.onAlarm.addListener(hourTrigger);
 chrome.runtime.onMessage.addListener(listenMessage);
+browser.storage.onChanged.addListener(storageChange);
 load();
