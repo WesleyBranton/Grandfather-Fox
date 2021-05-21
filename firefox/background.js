65eb4d72cbb65f174358c9dfc765c6ace9c5ee94
Wesley Branton <addons@wesleybranton.com>
2021-05-21 16:34:58 -0400

    Added support for custom audo (#2)

--- firefox/background.js
+++ firefox/background.js
@@ -64,13 +64,35 @@ function hourTrigger(alarmInfo) {
     } else if (hour == 0) {
         hour = 12;
     }
-    
-    audio = new Audio('audio/' + chimeName + '/' + hour + '.ogg');
-    audio.addEventListener('ended', audioEnded);
-    audio.addEventListener('pause', audioEnded);
-    audio.addEventListener('play', audioStarted);
-    audio.volume = chimeVolume;
-    audio.play();
+
+    if (chimeName == 'custom') {
+        try {
+            IDBFiles.getFileStorage({name: 'chimes'}).then((database) => {
+                database.get('chime_' + hour).then((data) => {
+                    if (data) {
+                        audio = new Audio(URL.createObjectURL(data));
+                        audio.addEventListener('ended', audioEnded);
+                        audio.addEventListener('pause', audioEnded);
+                        audio.addEventListener('play', audioStarted);
+                        audio.volume = chimeVolume;
+                        audio.play();
+                    } else {
+                        console.error('Error reading file:', 'File does not exist');
+                    }
+                });
+            });
+        } catch (error) {
+            console.error('Error reading file:', error);
+            return;
+        }
+    } else {
+        audio = new Audio('audio/' + chimeName + '/' + hour + '.ogg');
+        audio.addEventListener('ended', audioEnded);
+        audio.addEventListener('pause', audioEnded);
+        audio.addEventListener('play', audioStarted);
+        audio.volume = chimeVolume;
+        audio.play();
+    }
 }
 
 // Audio is playing
@@ -114,7 +136,207 @@ function storageChange(changes) {
     }
 }
 
+/**
+ * Validate file
+ * @param {File} file
+ * @returns Error
+ */
+function validateFile(file) {
+    const validTypes = ['audio/mpeg', 'audio/ogg', 'video/ogg', 'audio/wav'];
+    let isValidType = false;
+
+    for (let type of validTypes) {
+        if (file.type == type) {
+            isValidType = true;
+            break;
+        }
+    }
+
+    if (!isValidType) {
+        return 'Unsupported file type (Must be MP3, OGG, or WAV)';
+    }
+
+    if (file.size > 5000000) {
+        return 'File size exceeds limit (Maximum 5MB)';
+    }
+
+    return null;
+}
+
+/**
+ * Save file to database
+ * @param {String} filename
+ * @param {File} data
+ * @param {Object} port
+ */
+function saveFile(filename, data, port) {
+    try {
+        const validation = validateFile(data);
+        if (validation) {
+            throw validation;
+        }
+
+        IDBFiles.getFileStorage({name: 'chimes'}).then((database) => {
+            database.put(filename, data).then(() => {
+                port.postMessage({
+                    command: 'save',
+                    status: 'success'
+                });
+            });
+        });
+    } catch (error) {
+        console.error('Error saving file:', error);
+        port.postMessage({
+            command: 'save',
+            status: 'failed',
+            error: error
+        });
+    }
+}
+
+/**
+ * Load file from database
+ * @param {String} filename
+ * @param {Object} port
+ */
+function getFile(filename, port) {
+    try {
+        IDBFiles.getFileStorage({name: 'chimes'}).then((database) => {
+            database.get(filename).then((data) => {
+                if (data) {
+                    port.postMessage({
+                        command: 'load',
+                        status: 'success',
+                        file: data
+                    });
+                } else {
+                    console.error('Error reading file:', 'File does not exist');
+                    port.postMessage({
+                        command: 'load',
+                        status: 'failed',
+                        error: 'File not found'
+                    });
+                }
+            });
+        });
+    } catch (error) {
+        console.error('Error reading file:', error);
+        port.postMessage({
+            command: 'load',
+            status: 'failed',
+            error: error
+        });
+    }
+}
+
+/**
+ * Remove file from database
+ * @param {String} filename
+ * @param {Object} port
+ */
+function removeFile(filename, port) {
+    try {
+        IDBFiles.getFileStorage({name: 'chimes'}).then((database) => {
+            database.remove(filename).then((data) => {
+                port.postMessage({
+                    command: 'remove',
+                    status: 'success'
+                });
+            });
+        });
+    } catch (error) {
+        console.error('Error removing file:', error);
+        port.postMessage({
+            command: 'remove',
+            status: 'failed',
+            error: error
+        });
+    }
+}
+
+/**
+ * Get list of filenames from
+ * @param {Object} port
+ */
+function listFiles(port) {
+    try {
+        IDBFiles.getFileStorage({name: 'chimes'}).then((database) => {
+            database.list().then((list) => {
+                port.postMessage({
+                    command: 'list',
+                    status: 'success',
+                    list: list
+                });
+            });
+        });
+    } catch (error) {
+        console.error('Error listing files:', error);
+        port.postMessage({
+            command: 'list',
+            status: 'failed',
+            error: error
+        });
+    }
+}
+
+/**
+ * Remove all files from database
+ */
+function clearFiles() {
+    try {
+        IDBFiles.getFileStorage({name: 'chimes'}).then((database) => {
+            database.clear();
+        });
+    } catch (error) {
+        console.error('Error clearing files:', error);
+    }
+}
+
+/**
+ * Open connection to content script
+ * @param {Object} port
+ */
+ function registerPort(port) {
+    while (ports[port.name]) {
+        port.name = parseInt(port.name) + 1 + '';
+    }
+
+    ports[port.name] = port;
+    port.onDisconnect.addListener(unregisterPort);
+    port.onMessage.addListener(handlePortMessage);
+}
+
+/**
+ * Close connection to content script
+ * @param {Object} port
+ */
+function unregisterPort(port) {
+    delete ports[port.name];
+}
+
+function handlePortMessage(message, port) {
+    switch (message.command) {
+        case 'save':
+            saveFile(message.filename, message.file, port);
+            break;
+        case 'load':
+            getFile(message.filename, port);
+            break;
+        case 'remove':
+            removeFile(message.filename, port);
+            break;
+        case 'list':
+            listFiles(port);
+            break;
+        case 'clear':
+            clearFiles();
+            break;
+    }
+}
+
 var chimeName, chimeVolume, audio;
+const ports = {};
+browser.runtime.onConnect.addListener(registerPort);
 firstLoad();
 browser.alarms.onAlarm.addListener(hourTrigger);
 chrome.runtime.onMessage.addListener(listenMessage);
--- firefox/options/options.js
+++ firefox/options/options.js
@@ -2,7 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-// Save settings
+
+/**
+ * Save settings to Storage API
+ */
 function saveOptions() {
     browser.storage.local.set({
         'chime': chimeNoise.value
@@ -15,7 +18,9 @@ function saveOptions() {
     });
 }
 
-// Load options from storage
+/**
+ * Load settings from Storage API
+ */
 async function restoreOptions() {
     var setting = await browser.storage.local.get('chime');
     chimeNoise.value = setting.chime;
@@ -24,9 +29,21 @@ async function restoreOptions() {
     chimeVolume = setting.volume;
     volumeSlider.value = chimeVolume * 100;
     updateVolumeOutput();
+    toggleCustomAudio();
+    toggleCustomWarning();
+
+    if (chimeNoise.value == 'custom') {
+        updateCustomAudioList();
+    } else {
+        port.postMessage({
+            command: 'clear'
+        });
+    }
 }
 
-// Play audio sample
+/**
+ * Play preset audio sample
+ */
 function playAudio() {
     audio.pause();
     var audioType = chimeNoise.value;
@@ -35,23 +52,277 @@ function playAudio() {
     audio.play();
 }
 
-// Reload the alarm
+/**
+ * Reload the alarm
+ */
 function reload() {
     chrome.runtime.sendMessage('reload');
 }
 
-// Update volume output number
+/**
+ * Update volume output percentage label
+ */
 function updateVolumeOutput() {
     volumeOutput.textContent = volumeSlider.value + '%';
 }
+
+/**
+ * Toggle custom audio section based on user settings
+ */
+function toggleCustomAudio() {
+    const customChimeSection = document.getElementById('custom-chime-section');
+    const noCustomChimeSection = document.getElementById('no-custom-chime-section');
+
+    if (chimeNoise.value == 'custom') {
+        customChimeSection.classList.remove('hide');
+        noCustomChimeSection.classList.add('hide');
+    } else {
+        customChimeSection.classList.add('hide');
+        noCustomChimeSection.classList.remove('hide');
+    }
+}
+
+/**
+ * Handle button press for each custom audio
+ *     Removes chime if there's already audio set
+ *     Opens file selection dialog if there's no audio set
+ * @param {Event} event
+ */
+function triggerChimeUpdate(event) {
+    selectedHour = parseInt(event.target.id.split('-')[1]);
+
+    if (hoursWithChime[selectedHour]) { // Chime exists
+        removeChime();
+    } else { // No chime currently added
+        const input = document.getElementsByName('customChime')[0];
+        input.click();
+    }
+}
+
+/**
+ * Update buttons for custom audio hour
+ * @param {number} hour
+ * @param {boolean} hasAudio
+ */
+function updateCustomChimeUI(hour, hasAudio) {
+    const button = document.getElementById('custom-' + hour);
+    button.textContent = (hasAudio) ? 'Remove' : 'Add...';
+    button.classList.remove((hasAudio) ? 'default' : 'secondary');
+    button.classList.add((hasAudio) ? 'secondary' : 'default');
+
+    const previewButton = document.getElementById('listen-custom-' + hour);
+    previewButton.disabled = !hasAudio;
+
+    hoursWithChime[hour] = hasAudio;
+    selectedHour = null;
+    toggleCustomWarning();
+}
+
+/**
+ * Remove an existing custom audio
+ */
+function removeChime() {
+    toggleDialog(true);
+    
+    audio.pause();
+    onPreviewOver();
+
+    onSuccess = (message) => {
+        updateCustomChimeUI(selectedHour, false);
+    };
+
+    onFailed = (message) => {
+        selectedHour = null;
+        alert('Error removing chime:\n' + message.error);
+    };
+
+    port.postMessage({
+        command: 'remove',
+        filename: 'chime_' + selectedHour
+    });
+}
+
+/**
+ * Add custom audio
+ */
+function addChime() {
+    const input = document.getElementsByName('customChime')[0];
+    if (input.files.length != 1) {
+        return;
+    }
+
+    toggleDialog(true);
+
+    onSuccess = (message) => {
+        if (selectedHour != null) {
+            updateCustomChimeUI(selectedHour, true);
+        }
+    
+        document.getElementsByName('customChime')[0].value = null;
+    };
+
+    onFailed = (message) => {
+        document.getElementsByName('customChime')[0].value = null;
+        selectedHour = null;
+        alert('Error adding chime:\n' + message.error);
+    };
+
+    port.postMessage({
+        command: 'save',
+        filename: 'chime_' + selectedHour,
+        file: input.files[0]
+    });
+}
+
+/**
+ * Toggle preview for a custom chime
+ * @param {Event} event
+ */
+function previewChime(event) {
+    toggleDialog(true);
+
+    const button = event.target;
+    const hour = button.id.split('-')[2];
+    const isPlaying = button.classList.contains('playing');
+    
+    audio.pause();
+    onPreviewOver();
+
+    if (isPlaying) {
+        toggleDialog(false);
+        return;
+    }
+
+    onPreviewOver = () => {
+        button.classList.remove('playing');
+        button.textContent = 'Listen';
+    };
+
+    onSuccess = (message) => {
+        audio = new Audio(URL.createObjectURL(message.file));
+        audio.addEventListener('ended', onPreviewOver);
+        audio.volume = chimeVolume;
+        audio.play();
+        button.classList.add('playing');
+        button.textContent = 'Stop';
+    };
+
+    onFailed = (message) => {
+        alert('Error playing chime:\n' + message.error);
+    };
+
+    port.postMessage({
+        command: 'load',
+        filename: 'chime_' + hour
+    });
+}
+
+/**
+ * Bulk update the custom audio section
+ */
+function updateCustomAudioList() {
+    onSuccess = (message) => {
+        for (let file of message.list) {
+            const hour = parseInt(file.split('_')[1]);
+            updateCustomChimeUI(hour, true);
+        }
+        toggleCustomWarning();
+    };
+
+    onFailed = (message) => {
+        alert('Error loading chimes:\n' + message.error);
+    };
+
+    port.postMessage({
+        command: 'list'
+    });
+}
+
+/**
+ * Remove handlers for queued process
+ */
+function resetActionHandlers() {
+    onSuccess = () => {};
+    onFailed = () => {};
+}
+
+/**
+ * Handle incoming messages from background script
+ * @param {Object} message
+ */
+function processMessage(message) {
+    if (message.status == 'success') {
+        onSuccess(message);
+        toggleDialog(false);
+    } else {
+        onFailed(message);
+        toggleDialog(false);
+    }
+
+    resetActionHandlers();
+}
+
+/**
+ * Toggle loading dialog
+ * @param {boolean} show
+ */
+function toggleDialog(show) {
+    const dialog = document.getElementById('dialog-overlay');
+
+    if (show) {
+        dialog.classList.remove('hide');
+    } else {
+        dialog.classList.add('hide');
+    }
+
+}
+
+/**
+ * Show warning for custom chimes if an hour is missing
+ */
+function toggleCustomWarning() {
+    let show = false;
+
+    for (let hour of hoursWithChime) {
+        if (hour === false) {
+            show = true;
+        }
+    }
+
+    const warning = document.getElementById('chimes-not-set');
+    if (show) {
+        warning.classList.remove('hide');
+    } else {
+        warning.classList.add('hide');
+    }
+}
+
+// Place holder functions
+function onSuccess(message) {}
+function onFailed(message) {}
+function onPreviewOver() {}
+
+const port = browser.runtime.connect({name: Date.now() + ''});
+port.onMessage.addListener(processMessage);
 
 var chimeNoise = document.getElementById('chimeNoise');
 var volumeOutput = document.getElementById('volumeOutput');
 var volumeSlider = document.getElementById('chimeVolume');
 var audio = new Audio();
 var chimeVolume = 1;
+let selectedHour = null;
+let hoursWithChime = [0, false, false, false, false, false, false, false, false, false, false, false, false];
+
 restoreOptions();
+
 document.getElementById('sample').addEventListener('click', playAudio);
 document.getElementById('reload').addEventListener('click', reload);
 volumeSlider.addEventListener('input', updateVolumeOutput);
 document.settings.addEventListener('change', saveOptions);
+document.getElementsByName('customChime')[0].addEventListener('change', addChime);
+chimeNoise.addEventListener('change', toggleCustomAudio);
+
+for (let i = 1; i <= 12; i++) {
+    document.getElementById('custom-' + i).addEventListener('click', triggerChimeUpdate);
+    document.getElementById('listen-custom-' + i).addEventListener('click', previewChime);
+}
